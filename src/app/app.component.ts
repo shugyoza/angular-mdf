bcf0ebd751d98e17b3df045014e1970a193edfa1
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-06-03 09:03:04 -0700

    finished adding the getter methods and country class.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,7 +29,6 @@ export class AppComponent {
       isMarried: [''],
       country: [''],
     })
-
     // Creating a Nested FormGroup
     this.contactForm = this.formBuilder.group({
       firstName: ['', [Validators.required, Validators.minLength(10)]],
@@ -43,11 +42,70 @@ export class AppComponent {
         pinCode: ['', [Validators.required]]
       })
     })
-
   } // closing braces of the constructor
+
 
   get firstName() {
     return this.contactForm.get('firstName');
   }
 
+  get lastName() {
+    return this.contactForm.get('lastName');
+  }
+
+  get email() {
+    return this.contactForm.get('email');
+  }
+
+  get gender() {
+  return this.contactForm.get('gender');
+  }
+
+  get isMarried() {
+    return this.contactForm.get('isMarried');
+  }
+
+  get country() {
+    return this.contactForm.get('country');
+  }
+
+  get city() {
+    return this.contactForm.get('address').get('city');
+  }
+
+  get street() {
+    return this.contactForm.get('address').get('street');
+  }
+
+  get pinCode() {
+    return this.contactForm.get('address').get('pinCode');
+  }
+
+  getFieldValue(fieldLabel: string, subFieldLabel: string | undefined): string {
+    if (subFieldLabel === undefined) {
+      return this.contactForm.get(fieldLabel);
+    }
+    return this.contactForm.get(fieldLabel).get(subFieldLabel);
+  }
+
+  countryList: Country[] = [
+    new Country('1', 'India'),
+    new Country('2', 'USA'),
+    new Country('3', 'England')
+  ];
+
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
+
+} // closing braces of the component's class
+
+export class Country {
+  id: string;
+  name: string;
+
+  constructor(id: string, name: string) {
+    this.id = id;
+    this.name = name;
+  }
 }
